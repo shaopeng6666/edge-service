71c78b7f0e2d84d0d0b799d9696a8759019db2d3
zsp0817 <shaopeng0817@qq.com>
2025-06-19 14:19:12 +0800

    feat:chat11-authentication-fixBug

--- src/test/java/com/zsp/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/zsp/edgeservice/EdgeServiceApplicationTests.java
@@ -1,32 +1,35 @@
 package com.zsp.edgeservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class EdgeServiceApplicationTests {
 
     private static final int REDIS_PORT = 6379;
 
     @Container
-    static GenericContainer<?> redis =
-            new GenericContainer<>(DockerImageName.parse("redis:7.0"))
-                    .withExposedPorts(REDIS_PORT);
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
+            .withExposedPorts(REDIS_PORT);
+
+    @SuppressWarnings("removal")
+    @MockBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @DynamicPropertySource
     static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.redis.host", () -> redis.getHost());
-        registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+        registry.add("spring.data.redis.host", () -> redis.getHost());
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
     }
 
     @Test
